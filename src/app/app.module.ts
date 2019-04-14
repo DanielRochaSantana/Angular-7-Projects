8d7956da902c4d50a72a2aefca0f81306b632dfe
DanielRochaSantana <danibas92@hotmail.com>
2019-04-14 14:55:57 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AlertSucess01 } from './alertsucess.componet';
 import { AlertWarning02 } from './alertwarning.component';
+import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, NgbModule],
-  declarations: [AppComponent,ServerComponent, AlertSucess01, AlertWarning02],
+  declarations: [AppComponent,ServersComponent,ServerComponent, AlertSucess01, AlertWarning02],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
